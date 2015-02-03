e609e675d9e2789bd68c3afa0e39d9f87453e79d
Justin Painter <hpainter@gmail.com>
2015-02-03 09:35:40 -0500

    plans/index view template

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,7 +1,11 @@
 class PlansController < ApplicationController
+  layout "sidebar"
+  before_filter :require_login
 
   def index
-    #
+    #@plans = current_user.plans.all
+    @plans = [{},{},{},{},{}];
+    #@plans = [];
   end
 
   def new
